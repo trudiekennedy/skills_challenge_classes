fb5155301885b1b55763fd020aaaeb7aefda4529
trudie anderson <trudiekennedy@gmail.com>
2025-04-11 13:42:32 +0100

    Improving comments re: reading_chunk method on diary.py

--- lib/diary.py
+++ lib/diary.py
@@ -17,13 +17,18 @@ class DiaryEntry:
 
     def reading_chunk(self, wpm, mins):
         words_for_session = wpm * mins
+
+        # If we've read the whole text, reset words read back to zero to begin the chunk again.
         if self.words_read >= self.word_count:
             self.words_read = 0
         
+        # Find the index for our chunk of text to be read in session.
+        # Uses the indexes to get the chunk of words for our excerpt. 
         start = self.words_read
-        end = min(start + words_for_session, self.word_count)
-
+        end = min(start + words_for_session, self.word_count) # Makes sure word count isn't exceeded.
         excerpt = self.words[start:end]
+        
+        # Updates the words_read so that program knows how far the text has been read.
         self.words_read = end
 
         return " ".join(excerpt)
