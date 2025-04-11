4c2f99dabc7494e7c65a6994f3c89dcbc0b0a120
trudie anderson <trudiekennedy@gmail.com>
2025-04-11 14:24:31 +0100

    Updating grammar_stats.py with percentage_good method & including relevant tests

--- lib/grammar_stats.py
+++ lib/grammar_stats.py
@@ -2,15 +2,19 @@
 
 class GrammarStats:
     def __init__(self):
-        pass
+        self.passed_check = 0
+        self.failed_check = 0
 
     def check(self, text):
         if text == "":
             raise Exception("No text to analyse!")
-        return text[0].isupper() and text[-1] in ["!", "?", "."]
+        if text[0].isupper() and text[-1] in ["!", "?", "."]:
+            self.passed_check += 1
+            return True
+        else:
+            self.failed_check += 1
+            return False
 
     def percentage_good(self):
-        # Returns:
-        #   int: the percentage of texts checked so far that passed the check
-        #        defined in the `check` method. The number 55 represents 55%.
-        pass
+        total = self.passed_check + self.failed_check
+        return round(self.passed_check/total * 100)
--- tests/test_grammar_stats.py
+++ tests/test_grammar_stats.py
@@ -27,3 +27,35 @@ def test_incorrect_string_returns_false_with_caps_no_punctuation():
     grammar_stats = GrammarStats()
     result = grammar_stats.check("To be or not to be")
     assert result == False
+
+def test_return_percentage_when_one_is_good():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Shall I compare thee to a Summer's day?")
+    assert grammar_stats.percentage_good() == 100
+
+def test_return_percentage_when_three_are_good():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Fair is foul and foul is fair!")
+    grammar_stats.check("When shall we three meet again?")
+    grammar_stats.check("By the pricking of my thumbs, something wicked this way comes.")
+    assert grammar_stats.percentage_good() == 100
+
+def test_all_checks_fail():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("fair is foul and foul is fair!")
+    grammar_stats.check("When shall we three meet again")
+    grammar_stats.check("by the pricking of my thumbs, something wicked this way comes,")
+    assert grammar_stats.percentage_good() == 0
+
+def test_half_checks_fail():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Fair is foul and foul is fair!")
+    grammar_stats.check("When shall we three meet again")
+    assert grammar_stats.percentage_good() == 50
+
+def test_third_checks_fail():
+    grammar_stats = GrammarStats()
+    grammar_stats.check("Fair is foul and foul is fair!")
+    grammar_stats.check("When shall we three meet again")
+    grammar_stats.check("By the pricking of my thumbs, something wicked this way comes.")
+    assert grammar_stats.percentage_good() == 67
