d705e3d6d8a542feca4992410ff89e8bba029a46
trudie anderson <trudiekennedy@gmail.com>
2025-04-11 13:27:25 +0100

    refactoring diary.py to improve reset behaviour

--- lib/diary.py
+++ lib/diary.py
@@ -2,19 +2,14 @@ class DiaryEntry:
     def __init__(self, title, contents):
         self.title = title
         self.contents = contents 
-        self.word_count = 0
-        self.words =[]
+        self.words = contents.split()
+        self.word_count = len(self.words)
         self.words_read = 0
-        self.words_remaining = 0
 
     def format(self):
-        self.entry = f"{self.title}: {self.contents}"
-        return self.entry
+        return f"{self.title}: {self.contents}"
     
     def count_words(self):
-        self.words = self.contents.split()
-        self.word_count = len(self.words)
-        self.words_remaining = len(self.words)
         return self.word_count
 
     def reading_time(self, wpm):
@@ -22,17 +17,15 @@ class DiaryEntry:
 
     def reading_chunk(self, wpm, mins):
         words_for_session = wpm * mins
-        if self.words_remaining > 0:
-            excerpt = self.words[self.words_read:self.words_read + words_for_session]
-            self.words_read += len(excerpt)
-            self.words_remaining -= len(excerpt)
-            return " ".join(excerpt)
-        else:
-            self.words_remaining = self.word_count
+        if self.words_read >= self.word_count:
             self.words_read = 0
-            excerpt = self.words[:words_for_session]
-            self.words_read += len(excerpt)
-            self.words_remaining -= len(excerpt)
-            return " ".join(excerpt)
+        
+        start = self.words_read
+        end = min(start + words_for_session, self.word_count)
+
+        excerpt = self.words[start:end]
+        self.words_read = end
+
+        return " ".join(excerpt)
 
 
